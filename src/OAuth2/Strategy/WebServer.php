53a27aa864eeafa045c5df99e1c5faf94ad56dd7
Intrepidd <adrien@shopwiz.me>
2011-07-15 17:04:48 +0200

    Added basic error handling w/ exceptions

--- src/OAuth2/Strategy/WebServer.php
+++ src/OAuth2/Strategy/WebServer.php
@@ -11,19 +11,27 @@ class WebServer extends Base
     return parent::authorize_params(array_merge($options, array('type' => 'web_server')));
   }
   
- /**
-  * Retrieve an access token given the specified validation code.
-  * Note that you must also provide a <tt>:redirect_uri</tt> option
-  * in order to successfully verify your request for most OAuth 2.0
-  * endpoints.
-  */
+  /**
+   * Retrieve an access token given the specified validation code.
+   * Note that you must also provide a <tt>:redirect_uri</tt> option
+   * in order to successfully verify your request for most OAuth 2.0
+   * endpoints.
+   */
   public function get_access_token($code, $options = array())
   {
+    if (!$code) {
+      throw new \Exception("Invalid code provided");
+    }
+
     $response = $this->getClient()->request('POST', $this->getClient()->access_token_url(), $this->access_token_params($code, $options));
     
     $params = json_decode($response, true);
     if (!$params) parse_str($response, $params);
-    
+
+    if (!isset($params['access_token'])) {
+      throw new \Exception("Unable to retrieve access_token");
+    }
+
     $access = $params['access_token'];
     $refresh = (isset($params['refresh_token'])) ? $params['refresh_token'] : null ;
     $expires_in = (isset($params['expires_in'])) ? $params['expires_in'] : null ;
