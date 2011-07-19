d3370a9881b3993fe86c6de371f09a662d801c78
Félix Bellanger <felix.bellanger@gmail.com>
2011-07-19 13:13:06 +0200

    Moved things around

---
+++ autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+if (false === class_exists('Symfony\Component\ClassLoader\UniversalClassLoader', false)) {
+  require_once __DIR__.'/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+}
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->registerNamespaces(array(
+    'Symfony' => __DIR__.'/vendor'
+  , 'OAuth2'  => __DIR__.'/src',
+));
+$loader->registerPrefixes(array(
+  'EpiCurl_' => __DIR__.'/vendor/php-multi-curl',
+));
+$loader->register();
