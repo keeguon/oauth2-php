0662d75a48301fd498e737ef5339a8b8659041ba
Félix Bellanger <felix.bellanger@gmail.com>
2011-12-28 11:32:43 +0100

    Reverted the name change regarding the folder containing strategies and completed the Error class.

--- src/OAuth2/Error.php
+++ src/OAuth2/Error.php
@@ -4,6 +4,11 @@ namespace OAuth2;
 
 class Error extends \Exception
 {
+  protected
+      $code     = 0
+    , $message  = ''
+    , $response = null
+  ;
   
  /**
   * Construct the OAuth 2 error using a response object
@@ -12,7 +17,13 @@ class Error extends \Exception
   */
   public function __construct($response)
   {
-    
+    $response->error = $this;
+    $this->response  = $response;
+
+    $parsedResponse = $response->parse()
+    if (is_array($parsedResponse) {
+      $this->code    = $parsedResponse['error'];
+      $this->message = $parsedResponse['error_description'];
+    }
   }
-  
 }
